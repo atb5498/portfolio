ddb101c9902a5b1fe4776084d839d285bf7f7fb5
abickford <atb5498@gmail.com>
2020-02-18 23:11:23 -0600

    created header icons with tooltips

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Initializes tooltips
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Fades header on scroll
 $(window).scroll(function () {
     $("#header").css("opacity", 1 - $(window).scrollTop() / 250);
 });
