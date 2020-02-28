731955f3125424cfdee522ba80fe38241d57f2f1
abickford <atb5498@gmail.com>
2020-02-28 14:51:00 -0600

    fixed package.json

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import WebDev from "./components/WebDev/WebDev";
-import Design from "./components/Design/Design";
 
 function App() {
   return (
--- portfolio/src/components/WebDev/WebDev.js
+++ portfolio/src/components/WebDev/WebDev.js
@@ -18,19 +18,19 @@ function WebDev() {
                     <div className="row justify-content-center">
                         <div className="col-7 col-sm-6 col-md-4">
                             <a href="https://atb5498.github.io/brew-near-you/" target="_blank">
-                                <img class="project" src="images/brew-near-you.png" className="img-fluid" alt="Responsive image" />
+                                <img className="project" src="images/brew-near-you.png" className="img-fluid" alt="Responsive image" />
                             </a>
                         </div>
 
                         <div className="col-7 col-sm-6 col-md-4">
                             <a href="https://atb5498.github.io/elevate/" target="_blank">
-                                <img class="project" src="images/elevate.png" className="img-fluid" alt="Responsive image" />
+                                <img className="project" src="images/elevate.png" className="img-fluid" alt="Responsive image" />
                             </a>
                         </div>
 
                         <div className="col-7 col-sm-6 col-md-4">
                             <a href="https://atb5498.github.io/work-day-planner/" target="_blank">
-                                <img class="project" src="images/work-day-planner.png" className="img-fluid" alt="Responsive image" />
+                                <img className="project" src="images/work-day-planner.png" className="img-fluid" alt="Responsive image" />
                             </a>
                         </div>
                     </div>
