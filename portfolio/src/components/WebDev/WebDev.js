4a90aebba5016060a622cf215b4bde01e5b226ff
abickford <atb5498@gmail.com>
2020-03-19 15:08:02 -0500

    updated projects

--- portfolio/src/components/WebDev/WebDev.js
+++ portfolio/src/components/WebDev/WebDev.js
@@ -17,6 +17,14 @@ function WebDev() {
                 <div id="projects">
                     <div className="row justify-content-center text-center">
                         <div id="project-1" className="col-7 col-sm-6 col-md-4">
+                            <p>Plan your day.</p>
+
+                            <a href="https://atb5498.github.io/work-day-planner/" target="_blank">
+                                <img id="project3" src="images/work-day-planner.png" className="project img-fluid" alt="Responsive image" />
+                            </a>
+                        </div>
+
+                        <div id="project-2" className="col-7 col-sm-6 col-md-4">
                             <p>Find a brewery.</p>
 
                             <a href="https://atb5498.github.io/brew-near-you/" target="_blank">
@@ -24,19 +32,11 @@ function WebDev() {
                             </a>
                         </div>
 
-                        <div id="project-2" className="col-7 col-sm-6 col-md-4">
-                            <p>Generate a profile.</p>
-
-                            <a href="https://atb5498.github.io/elevate/" target="_blank">
-                                <img src="images/elevate.png" className="project img-fluid" alt="Responsive image" />
-                            </a>
-                        </div>
-
                         <div id="project-3" className="col-7 col-sm-6 col-md-4">
-                            <p>Plan your day.</p>
+                            <p>Sell your books.</p>
 
-                            <a href="https://atb5498.github.io/work-day-planner/" target="_blank">
-                                <img id="project3" src="images/work-day-planner.png" className="project img-fluid" alt="Responsive image" />
+                            <a href="https://the-bookup.herokuapp.com/" target="_blank">
+                                <img src="images/the-bookup.png" className="project img-fluid" alt="Responsive image" />
                             </a>
                         </div>
                     </div>
